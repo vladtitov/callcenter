bcdefa85c6e90b9d4902d969c7d8ba13b9e1ed94
GuencoYuriy <yrik69@mail.ru>
2016-05-08 23:26:55 +0300

    OneIcon2

---
+++ js/base.js
@@ -0,0 +1,11 @@
+/**
+ * Created by VladHome on 3/19/2016.
+ **/
+///<reference path="../typings/jquery.d.ts"/>
+///<reference path="../typings/underscore.d.ts"/>
+///<reference path="../typings/require.d.ts"/>
+///<reference path="../typings/backbone-global.d.ts"/>
+///<reference path="com/BackboneTable.ts"/>
+///<reference path="com/RowModel.ts"/>
+///<reference path="com/OneIcon.ts"/>
+//# sourceMappingURL=base.js.map
